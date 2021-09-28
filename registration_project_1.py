8a4dd03540c9bb545b6c91d8b559b8393d209a75
PaulineHaulez <91547478+PaulineHaulez@users.noreply.github.com>
2021-09-28 16:02:37 +0200

    Add files via upload

---
+++ registration_project_1.py
@@ -0,0 +1,103 @@
+"""
+Project code for image registration topics.
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import registration as reg
+from IPython.display import display, clear_output
+
+
+def intensity_based_registration_demo(I, Im, method, check, lr):
+
+    # read the fixed and moving images
+    # change these in order to read different images
+
+    # initial values for the parameters
+    # we start with the identity transformation
+    # most likely you will not have to change these
+    
+    # NOTE: for affine registration you have to initialize
+    # more parameters and the scaling parameters should be
+    # initialized to 1 instead of 0
+
+    # the similarity function
+    # this line of code in essence creates a version of rigid_corr()
+    # in which the first two input parameters (fixed and moving image)
+    # are fixed and the only remaining parameter is the vector x with the
+    # parameters of the transformation
+    
+    if check=="CNN":
+        if method=="rigid":
+            x = np.array([0., 0., 0.])
+            fun = lambda x: reg.rigid_corr(I, Im, x, return_transform=False)
+        if method=="affine":
+            x = np.array([0., 1., 1., 0., 0., 0., 0.])
+            fun = lambda x: reg.affine_corr(I, Im, x, return_transform=False)
+    if check=="MI":
+        if method=="affine":
+            x = np.array([0., 1., 1., 0., 0., 0., 0.])
+            fun = lambda x: reg.affine_mi(I, Im, x, return_transform=False)
+        
+    # the learning rate
+    mu = lr
+
+    # number of iterations
+    num_iter = 200
+
+    iterations = np.arange(1, num_iter+1)
+    similarity = np.full((num_iter, 1), np.nan)
+
+    fig = plt.figure(figsize=(20,3))
+
+    # fixed and moving image, and parameters
+    ax1 = fig.add_subplot(121)
+
+    # fixed image
+    im1 = ax1.imshow(I)
+    # moving image
+    im2 = ax1.imshow(I, alpha=0.7)
+    # parameters
+    txt = ax1.text(0.3, 0.95,
+        np.array2string(x, precision=5, floatmode='fixed'),
+        bbox={'facecolor': 'white', 'alpha': 1, 'pad': 10},
+        transform=ax1.transAxes)
+
+    # 'learning' curve
+    ax2 = fig.add_subplot(122, xlim=(0, num_iter), ylim=(0, 1))
+
+    learning_curve, = ax2.plot(iterations, similarity, lw=2)
+    ax2.set_xlabel('Iteration')
+    ax2.set_ylabel('Similarity')
+    ax2.grid()
+
+    # perform 'num_iter' gradient ascent updates
+    for k in np.arange(num_iter):
+
+        # gradient ascent
+        g = reg.ngradient(fun, x)
+        x += g*mu
+
+        # for visualization of the result
+        if check=="CNN":
+            if method=="rigid":
+                S, Im_t, _ = reg.rigid_corr(I, Im, x, return_transform=True)
+            if method=="affine":
+                S, Im_t, _ = reg.affine_corr(I, Im, x, return_transform=True)
+        if check=="MI": 
+            if method=="affine":
+                S, Im_t, _ = reg.affine_mi(I, Im, x, return_transform=True) 
+        
+
+        clear_output(wait = True)
+
+        # update moving image and parameters
+        im2.set_data(Im_t)
+        txt.set_text(np.array2string(x, precision=5, floatmode='fixed'))
+
+        # update 'learning' curve
+        similarity[k] = S
+        learning_curve.set_ydata(similarity)
+
+        display(fig)
+        
